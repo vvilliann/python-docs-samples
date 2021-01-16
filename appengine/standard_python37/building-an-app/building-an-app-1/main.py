79b3082aad43b7c33f735a6db373905e2b9cf13a
Leiniao1 <yinhangtsinghua@gmail.com>
2021-01-15 17:46:02 -0800

    Update main.py

--- appengine/standard_python37/building-an-app/building-an-app-1/main.py
+++ appengine/standard_python37/building-an-app/building-an-app-1/main.py
@@ -14,6 +14,7 @@
 
 # [START gae_python37_render_template]
 import datetime
+import random
 
 from flask import Flask, render_template, request
 
@@ -25,10 +26,47 @@ usertable = {
 }
 
 words = [
-    "小米"
+    ["小米", "大米"],
+    ["任天堂Switch", "索尼PS5"],
+    ["沙尘暴", "雾霾"],
+    ["亚利桑那", "路易斯安那"],
+    ["波多黎各", "关岛"],
+    ["法拉第", "特斯拉"],
+    ["伏特加", "朗姆酒"],
+    ["微软", "亚马逊"],
+    ["海参", "海星"],
+    ["引力波", "黑洞"],
+    ["宠物小精灵", "皮卡丘"],
+    ["多山", "地震"],
+    ["跨性别者", "双性恋"],
+    ["列宁", "恩格斯"],
+    ["刘邦", "陈胜"],
+    ["金瓶梅", "西厢记"],
+    ["白鹭", "天鹅"],
+    ["Napa", "Mission Peak"],
+    ["无人机", "大疆"],
+    ["小白", "Jefferey Zhao"],
+    ["王维", "李白"],
+    ["腰果", "香蕉"],
+    ["绿宝石", "翡翠"],
+    ["南北朝", "五代十国"],
+    ["商朝", "周朝"],
+    ["自由女神", "圣女贞德"],
+    ["趾甲", "指甲"],
+    ["石膏", "大理石"],
+    ["维纳斯", "雅典娜"],
+    ["吸血鬼", "蚊子"],
 ]
 
+identity = [0, 1] # 1 is spy, 0 is normal, 2 is idiot
+identity_table = {
+    "dummy_name" : 0,
+    "dummy_spy" : 1,
+}
+
 peoplenum = 7
+spynum = 2
+seed = 0
 
 @app.route('/')
 def root():
@@ -53,19 +91,19 @@ def login():
       password = request.form['password']
       if (user in usertable):
           if (usertable[user] != password):
-              return render_template('index.html', word="", randomseed=0, user="Login fail!", password="Password wrong!", peoplenum=peoplenum)
+              return render_template('index.html', word="", randomseed=seed, user="Login fail!", password="Password wrong!", peoplenum=peoplenum)
       else:
           usertable[user] = password
-      return render_template('index.html', word="", randomseed=0, user=user, password=password, peoplenum=peoplenum)
+      return render_template('index.html', word="", randomseed=seed, user=user, password=password, peoplenum=peoplenum)
    else:
       user = request.args['name']
       password = request.args['password']
       if (user in usertable):
           if (usertable[user] != password):
-              return render_template('index.html', word="", randomseed=0, user="Login fail!", password="Password wrong!", peoplenum=peoplenum)
+              return render_template('index.html', word="", randomseed=seed, user="Login fail!", password="Password wrong!", peoplenum=peoplenum)
       else:
           usertable[user] = password
-      return render_template('index.html', word="", randomseed=0, user=user, password=password, peoplenum=peoplenum)
+      return render_template('index.html', word="", randomseed=seed, user=user, password=password, peoplenum=peoplenum)
 
 @app.route('/setupgame/<user>/<password>', methods=['POST'])
 def setupgame(user, password):
@@ -76,9 +114,23 @@ def setupgame(user, password):
         return render_template('index.html', word="", randomseed=0, user="Login fail!", password="User not exist!", peoplenum=peoplenum)
     
     peoplenum = request.form['peoplenum']
+    spynum = request.form['spynum']
+    currsize = len(words)
+    seed = random.randint(0, currsize - 1)
+    
+    # Set up identity
+    identity = [0] * (peoplenum + 10)
+    identity_table = {}
+    for iter in range(spynum):
+        humanseed = random.randint(0, peoplenum - 1)
+        while (identity[humanseed] != 0):
+            humanseed = random.randint(0, peoplenum - 1)
+        identity[humanseed] = 1
+    
+    seed_human = random.randint()
     # [END submitted]
     # [START render_template]
-    return render_template('index.html', word="", randomseed=0, user=user, password=password, peoplenum=peoplenum)
+    return render_template('index.html', word="", randomseed=seed, user=user, password=password, peoplenum=peoplenum)
     # [END render_template]
 
 @app.route('/receiveword/<user>/<password>', methods=['POST'])
@@ -88,12 +140,16 @@ def receiveword(user, password):
               return render_template('index.html', word="", randomseed=0, user="Login fail!", password="Password wrong!", peoplenum=peoplenum)
     else:
         return render_template('index.html', word="", randomseed=0, user="Login fail!", password="User not exist!", peoplenum=peoplenum)
-    word = "小米"
-    randomseed = 100
+    
+    if !(user in identity_table):
+        identity_table[user] = identity[len(identity_table) - 1]
+    
+    your_identity = identity_table[user]
+    word = words[seed][your_identity]
     
     # [END submitted]
     # [START render_template]
-    return render_template('index.html', word=word, randomseed=randomseed, user=user, password=password, peoplenum=peoplenum)
+    return render_template('index.html', word=word, randomseed=seed, user=user, password=password, peoplenum=peoplenum)
     # [END render_template]
 
 @app.route('/changeword/<user>/<password>', methods=['POST'])
@@ -103,12 +159,24 @@ def changeword(user, password):
               return render_template('index.html', word="", randomseed=0, user="Login fail!", password="Password wrong!", peoplenum=peoplenum)
     else:
         return render_template('index.html', word="", randomseed=0, user="Login fail!", password="User not exist!", peoplenum=peoplenum)
-    word = "大米"
-    randomseed = 200
+    
+    # Set up identity
+    identity = [0] * (peoplenum + 10)
+    identity_table = {}
+    for iter in range(spynum):
+        humanseed = random.randint(0, peoplenum - 1)
+        while (identity[humanseed] != 0):
+            humanseed = random.randint(0, peoplenum - 1)
+        identity[humanseed] = 1
+    if !(user in identity_table):
+        identity_table[user] = identity[len(identity_table) - 1]
+    
+    your_identity = identity_table[user]
+    word = words[seed][your_identity]
     
     # [END submitted]
     # [START render_template]
-    return render_template('index.html', word=word, randomseed=randomseed, user=user, password=password, peoplenum=peoplenum)
+    return render_template('index.html', word=word, randomseed=seed, user=user, password=password, peoplenum=peoplenum)
     # [END render_template]
 
 if __name__ == '__main__':
