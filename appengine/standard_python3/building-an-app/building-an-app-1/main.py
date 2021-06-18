27b49950b93ae482ab51d638820b1d80c2025a71
vvilliann <65872216+vvilliann@users.noreply.github.com>
2021-06-18 14:09:28 +0800

    Update main.py

--- appengine/standard_python3/building-an-app/building-an-app-1/main.py
+++ appengine/standard_python3/building-an-app/building-an-app-1/main.py
@@ -19,7 +19,8 @@ import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+variable_dict = {}
+log = ""
 
 @app.route('/')
 def root():
@@ -34,19 +35,91 @@ def root():
 
 @app.route('/submit', methods=['POST'])
 def submit_textarea():
-    return "You entered: {}".format(request.form["usercode"])
+    input_str = request.form["usercode"]
+    operator(input_str)
+    return "Dynamics: \n" + log
 
-def findIntDefination(input_str):
+def operator(input_str):
+    code_in_for_loop = ""
+    target_iter = 1
+    iter_value = -1
+    global variable_dict
+    for line in input_str.split('\n'):
+        variable, init_value = findIntDefinition(line)
+        loop_iter = findForLoop(line)
+        loop_end = findForLoopEnd(line)
+        plus = findPlusOperation(line)
+        if (variable != ""):
+            variable_dict[variable] = init_value
+            reporter()
+        elif (loop_iter > -1):
+            iter_value = 0
+            target_iter = loop_iter
+        elif (loop_end > -1):
+            for index in range(target_iter):
+                operator(code_in_for_loop)
+            code_in_for_loop = ""
+            target_iter = 1
+            iter_value = -1
+        elif (iter_value > -1):
+            code_in_for_loop += line + "\n"
+        elif (plus != "" and plus in variable_dict):
+            variable_dict[plus] = variable_dict[plus] + 1
+            reporter()
+
+def reporter():
+    global log
+    log = log + "===========================================\n"
+    for variable in variable_dict:
+        log = log + "[" + variable + "|" + str(variable_dict[variable]) + "]\n"
+    log = log + "===========================================\n"
+
+def findIntDefinition(input_str):
     p = re.compile(r'int \w+ = \d+;')
     m = p.match(input_str)
     if m == None:
-        return '', -1
+        return "", -1
     p1 = re.compile(r'=')
     m1 = p1.search(input_str)
     equal_index = m1.start()
-    variable = input_str[4:equal_index - 1]
-    init_value = input_str[equal_index+2:input_str.len() - 1]
-    return variable, init_value
+    p2 = re.compile(r';')
+    m2 = p2.search(input_str)
+    pointer2_index = m2.start()
+    variable = input_str[4:equal_index-1]
+    init_value = input_str[equal_index+2:pointer2_index]
+    return variable, int(init_value)
+
+def findForLoop(input_str):
+    p = re.compile(r'for \(int \w+ = 0; \w+ < \d+; \w+\+\+\) \{')
+    m = p.match(input_str)
+    if m == None:
+        return -1
+    p1 = re.compile(r'<')
+    m1 = p1.search(input_str)
+    pointer1_index = m1.end()
+    p2 = re.compile(r'; \w+\+\+\)')
+    m2 = p2.search(input_str)
+    pointer2_index = m2.start()
+    iter_value = input_str[pointer1_index+1:pointer2_index]
+    return int(iter_value)
+
+def findForLoopEnd(input_str):
+    p = re.compile(r'\}')
+    m = p.match(input_str)
+    if m == None:
+        return -1
+    return 1
+
+def findPlusOperation(input_str):
+    p = re.compile(r'\w+\+\+;')
+    m = p.match(input_str)
+    if m == None:
+        return ""
+    p1 = re.compile(r'\+\+')
+    m1 = p1.search(input_str)
+    plus_index = m1.start()
+    variable = input_str[:plus_index]
+    return variable
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
